1d165456f8a00edec39872cb5a82c2a51b6c72ac
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-04-23 16:22:09 -0300

    Add animations and hover effects in Gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,33 @@
 import data from '../api/projects.js';
 
 function Gallery() {
-    const projects = data[0].projects
-    const gridItems = projects.map((projectItem) => {
 
-    const projectThumb = projectItem.covers.original;
-        return (
-            <div className="gallery__item">
-                <img src={projectThumb} alt="" />
-            </div>
-        );
-    });
+    function getAnimationDelay(item) {
+        const milliseconds = "ms";
+        const animationTimeDefault = 500;
+        const animationDelay = (item * animationTimeDefault) + animationTimeDefault;
+        return (animationDelay + milliseconds);
+    }
+    const projects = data[0].projects;
     
+    const gridItems = projects.map((projectItem, keyProject) => {
+        const animationDelay = getAnimationDelay(keyProject);
+
+        const projectThumb = projectItem.covers.original;
+            return (
+                <div style={{animationDelay: animationDelay}} className="gallery__item" >
+                    <span className="grid__modal">
+                        <h4 className="modal__title">{projectItem.name}</h4>
+                        <p className="modal__category">
+                            <a href={projectItem.url} target="_blank" rel="noreferrer noopener">
+                                <i class="fa-brands fa-behance"></i>Click here
+                            </a></p>
+                    </span>
+                    <img src={projectThumb} alt="" />
+                </div>
+            );
+    });
+
     return (
         <div className="gallery">
             {gridItems}
