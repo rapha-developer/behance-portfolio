a268f978655c7e3e8cc03385660e83c8871cf54f
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-20 09:54:25 -0300

    Add [href] property to poster link and blog link when clicked in image and title; And add hover effects in titles

--- src/components/Grid/Blogs.jsx
+++ src/components/Grid/Blogs.jsx
@@ -7,10 +7,15 @@ function Blogs(props) {
             <div className={`blogs__item ${classInvert}`} key={key} >
                 <div className="blogs__column blogs__image" >
                     <img src={blogItem.image} alt="Alt Text Image" />
-
-                    <div className="blogs__thumb">
-                        <i className="fa-solid fa-up-right-from-square"></i>
-                    </div>
+                    <a 
+                        href={blogItem.url} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="posts__link" >
+                        <div className="blogs__thumb">
+                            <i className="fa-solid fa-up-right-from-square"></i>
+                        </div>
+                    </a>
                 </div>
                 <div className="blogs__column blogs__column--gap">
                     <div className="blogs__dateWrapper">
@@ -18,7 +23,13 @@ function Blogs(props) {
                     </div>
                     <div className="blogs__card">
                         <div className="blogs__content">
-                            <h3 className="blogs__title">{blogItem.title}</h3>
+                            <a 
+                                href={blogItem.url} 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="posts__link" >
+                                <h3 className="blogs__title">{blogItem.title}</h3>
+                            </a>
                             <span className="blogs__author">By {blogItem.author}</span>
                             <p className="blogs__text">{blogItem.text}</p>
                         </div>
--- src/components/Grid/Posts.jsx
+++ src/components/Grid/Posts.jsx
@@ -1,35 +1,6 @@
 
-function Posts() {
-    const posts = [
-        {
-            date: "nOvEMbeR 9, 2016",
-            title: "Top 3 best Commerce Websites in 2016",
-            author: "David Butter",
-            text: "Founded in 2014, by designers Leonard, OrionStudio is a global brand and design agency with offices in the creative hubs of London and San Francisco.",
-            image: "https://themes.g5plus.net/orion/creative-agency/wp-content/uploads/2016/09/post-04-646x510.jpg",
-        },
-        {
-            date: "nOvEMbeR 9, 2016",
-            title: "The 10 best Web design in November",
-            author: "David Butter",
-            text: "Founded in 2014, by designers Leonard, OrionStudio is a global brand and design agency with offices in the creative hubs of London and San Francisco.",
-            image: "https://themes.g5plus.net/orion/creative-agency/wp-content/uploads/2016/11/blog-02-646x510.jpg",
-        },
-        {
-            date: "nOvEMbeR 8, 2016",
-            title: "The 10 best Web design in November",
-            author: "David Butter",
-            text: "Founded in 2014, by designers Leonard, OrionStudio is a global brand and design agency with offices in the creative hubs of London and San Francisco.",
-            image: "https://themes.g5plus.net/orion/creative-agency/wp-content/uploads/2016/11/blog-03-646x510.jpg",
-        },
-        {
-            date: "nOvEMbeR 7, 2016",
-            title: "The 10 best new graphic design tools",
-            author: "David Butter",
-            text: "Founded in 2014, by designers Leonard, OrionStudio is a global brand and design agency with offices in the creative hubs of London and San Francisco.",
-            image: "https://themes.g5plus.net/orion/creative-agency/wp-content/uploads/2016/11/blog-04-646x510.jpg",
-        },
-    ]
+function Posts(props) {
+    const posts = props.posts
     const posts__items = posts.map((postItem, key) => {
         const invertClass =  ((key + 1) % 2 === 0) ?  'invert' : '' 
         return (
@@ -37,14 +8,27 @@ function Posts() {
                 <div className="posts__column post__image">
                     <img src={postItem.image} alt="Image Alt Text" />
 
-                    <div className="posts__thumb">
-                        <i className="fa-solid fa-up-right-from-square"></i>
-                    </div>
+                    <a 
+                        href={postItem.url} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="posts__link" >
+                        <div className="posts__thumb">                        
+                            <i className="fa-solid fa-up-right-from-square"></i>
+                        </div>
+                    </a>
+
                 </div>
                 <div className="posts__column post__column--gap">
                     <div className="posts__content">
                         <h6 className="posts__date uppercase">{postItem.date}</h6>
-                        <h3 className="posts__title">{postItem.title}</h3>
+                        <a 
+                        href={postItem.url} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="posts__link" >
+                            <h3 className="posts__title">{postItem.title}</h3>
+                        </a>
                         <span className="posts__author capitalize">By {postItem.author}</span>
                         <p className="posts__text">{postItem.text}</p>
                     </div>
