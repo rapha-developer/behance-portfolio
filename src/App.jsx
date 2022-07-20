35bdf67a81d13bdd82048969fa4ff3f3ac8c86bf
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-20 10:45:08 -0300

    Add BrowserRouter and Routes components necessary to config React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import './styles/fonts/orion.fonts.css'
 import './App.css'
@@ -9,18 +10,22 @@ import About from './pages/About'
 import PortfolioInBlogs from './pages/PortfolioInBlogs'
 import PortfolioInGallery from './pages/PortfolioInGallery'
 import PortfolioInPosts from './pages/PortfolioInPosts'
+import Navbar from './components/Menu/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      {/* <Home /> */}
-      {/* <PortfolioInPosts /> */}
-      {/* <PortfolioInBlogs /> */}
-      {/* <PortfolioInGallery /> */}
-      {/* <About /> */}
-      {/* <Contact /> */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="posts" element={<PortfolioInPosts />} />
+        <Route path="gallery" element={<PortfolioInGallery />} />
+        <Route path="blogs" element={<PortfolioInBlogs />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
