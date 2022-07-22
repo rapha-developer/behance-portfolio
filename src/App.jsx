a8bdb640f79185d9cce64fda9b8f394de2c5ca46
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-21 22:19:46 -0300

    Map all projects data; and make project__list objects from ProjectModel

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,34 @@ import PortfolioInGallery from './pages/PortfolioInGallery'
 import PortfolioInPosts from './pages/PortfolioInPosts'
 import Navbar from './components/Menu/Navbar'
 
+import projects from './api/projects'
+import {ProjectModel} from './Helper/ProjectModel'
+
 function App() {
   const [count, setCount] = useState(0)
+  const projects_list = projects.map((projectItem) => {
+
+    const project = new ProjectModel(projectItem)
+    return {
+      id: project.id,
+      date: project.date,
+      title: project.title,
+      author: project.author,
+      text: project.text,
+      tags: project.tags,
+      url: project.url,
+      image: project.image
+    }
+  });
 
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="posts" element={<PortfolioInPosts />} />
-        <Route path="gallery" element={<PortfolioInGallery />} />
-        <Route path="blogs" element={<PortfolioInBlogs />} />
+        <Route path="posts" element={<PortfolioInPosts projects={projects_list} />} />
+        <Route path="gallery" element={<PortfolioInGallery projects={projects_list} />} />
+        <Route path="blogs" element={<PortfolioInBlogs projects={projects_list} />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
