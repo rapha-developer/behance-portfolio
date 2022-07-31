7610906ed7fbab0dca1377d6938969bd3c58d3d0
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-31 17:32:46 -0300

    Add new file contactInformation and pass to fill Contact Page and ContactInfo Component

---
+++ src/api/contactInformation.js
@@ -0,0 +1,29 @@
+export default {
+    intro: "contact us",
+    title: "Entre contato por:",
+    contactList: [
+        {   type: "Email", 
+            icon: "fa-regular fa-paper-plane", 
+            url: "mailto:contatosraphaelmoreira@gmail.com",
+            value:"contatosraphaelmoreira@gmail.com" 
+        },
+        {   
+            type: "Github", 
+            icon: "fa-brands fa-github", 
+            url: "https://github.com/rapha-developer", 
+            value: "@github.com/rapha-developer" 
+        },
+        { 
+            type: "Codepen", 
+            icon: "fa-brands fa-codepen", 
+            url: "https://codepen.io/rapha-developer", 
+            value: "@codepen.io/rapha-developer" 
+        },
+        { 
+            type: "Behance", 
+            icon: "fa-brands fa-behance", 
+            url: "https://www.behance.net/rapha-developer", 
+            value: "@behance.io/rapha-developer" 
+        },
+    ]
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,21 +2,13 @@ import ContactInfo from "../components/ContactInfo"
 import LocationMap from "../components/LocationMap"
 import Poster from "../components/Poster"
 
+import contactInformation from "../api/contactInformation"
 function Contact() {
     const posterData = {
         title: "contact",
         subtitle: "home ~ contact"
     }
-    const contact = {
-        intro: "contact us",
-        title: "Entre contato por:",
-        contactList: [
-            { type: "Email", icon: "fa-regular fa-paper-plane", value:"contatosraphaelmoreira@gmail.com" },
-            { type: "Github", icon: "fa-brands fa-github", value: "@github.com/rapha-developer" },
-            { type: "Codepen", icon: "fa-brands fa-codepen", value: "@codepen.io/rapha-developer" },
-            { type: "Behance", icon: "fa-brands fa-behance", value: "@behance.io/rapha-developer" },
-        ]
-    }
+    const contact = contactInformation
     return (
         <div className="contact">
             <Poster  
