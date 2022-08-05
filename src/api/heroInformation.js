e3b7dee9b4fbcd549f8a88246854070511c70998
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-08-04 21:21:35 -0300

    Add informations necessary to show in Home Page

---
+++ src/api/heroInformation.js
@@ -0,0 +1,14 @@
+export default {
+    user: {
+        name: "Raphael Moreira",
+        employer: "Creator",
+        work: "Frontend developer",
+    },
+    description: "Hi, my name is Raphael Moreira and I'm a Creator & Frontend developer based in São Paulo, Brazil. I publish over 10 years of experience in the design industry. I'm specialized in Front and  backend development. You can check out my work below",
+    description_es: "Hi, my name is Raphael Moreira and I'm a Creator & Frontend developer based in São Paulo, Brazil. I publish many components and pages on my Behance and Codepen.io profile. I have a degree in Computer Science. You can check out my work below:",
+    description_pt:"Eu publico muitos components e páginas no meu perfil do Behance e Codepen.io. Eu tenho formação superior em Ciências da Computação. Você pode descobrir meu trabalho abaixo: ",
+    image: {
+        gravatar_url: "https://avatars.githubusercontent.com/u/91702283?v=4",
+        alt_text: "Photo github profile"
+    }
+}
